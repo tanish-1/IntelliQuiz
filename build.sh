4bce93b8b03e3bf91d5bddcca9a34b083d2bd4ce
tanish-1 <imtanishlamba.com>
2025-04-17 21:09:42 +0530

    changes applied

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -117,9 +117,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+STATICFILES_DIRS=[STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL='/afterlogin'
 
